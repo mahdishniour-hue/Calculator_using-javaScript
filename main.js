d198bc957d016c31143d3b149d747e1ddaca5a32
mahdishniour-hue <mahdishniour@gmail.com>
2026-05-17 00:22:36 +0300

    Add keyboard input handling for calculator

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 let displayNum = document.getElementById('display');
+let listMathKey = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "/", "*", "+", "-"];
+
+window.onload = function () {
+    displayNum.focus();
+}
 
 function appenToAdd(num) {
     displayNum.value += num;
@@ -16,6 +21,22 @@ function calc() {
         displayNum.value = 'Erorr'
     };
 };
+
 function backSpace() {
     displayNum.value = displayNum.value.slice(0, -1);
 }
+
+displayNum.addEventListener("keydown", function (num) {
+    if (num.key == "Enter" || num.key == "=") {
+        return calc();
+    }
+    if (num.key == "Delete") {
+        return clearDisplay();
+    }
+    if (num.key == "Backspace") {
+        return displayNum.value = displayNum.value.slice(0, -1);
+    }
+    if (listMathKey.includes(num.key)) {
+        displayNum.value += num.key;
+    }
+})
